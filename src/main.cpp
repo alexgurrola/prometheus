2771cf58b457343f067dc46283e94a3e717461b9
alexgurrola <alexgurrola1@gmail.com>
2016-05-08 18:55:22 -0700

    Bug Fixes / JSON Prototype

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,23 @@
 #include "../versilis/cpu.h"
+#include "../versilis/json.h"
 #include <iostream>
 
 using namespace std;
 
 int main() {
+
+    // JSON Test
+    json *JSON = new json();
+    char testCharacters[] = {"testing"};
+
+    cout << 'Bool: ' << JSON->stringify(true) << endl;
+    cout << 'Int: ' << JSON->stringify(2) << endl;
+    //cout << 'Double: ' << JSON->stringify(1.5) << endl;
+    cout << 'String: ' << JSON->stringify(testCharacters) << endl;
+
+    // CPU Test
     cpu();
+
+    // Return
     return 0;
 }
